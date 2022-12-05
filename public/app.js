05a814e08ce47b40cdae07e515ccf0439b204a1e
imubarak234 <imubarak2424@gmail.com>
2022-12-05 12:04:40 +0100

    app.client.request

--- public/app.js
+++ public/app.js
@@ -44,5 +44,43 @@ app.client.request = function(headers, path, method, queryStringObject, payload,
   // Form the http request as a JSON type
   let xhr = new XMLHttpRequest();
   xhr.open(method, requestUrl, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  // For each header sent, add it to the request
+  for (let headerKey in headers){
+
+    if(headers.hasOwnProperty(headerKey)){
+      xhr.setRequestHeader(headerKey, headers[headerKey]);
+    }
+  }
+
+
+  // If there is a current session token set, add that as a header
+  if(app.config.sessionToken){
+    xhr.setRequestHeader("token", app.config.sessionToken.id);
+  }
+
+  // When the request comes back, handle the response
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState == XMLHttpRequest.DONE){
+      let statusCode = xhr.status;
+      let responseReturned = xhr.responseText;
+
+      // Callback if requested
+      if (callback){
+        try{
+          let parsedResponse = JSON.parse(responseReturned);
+          callback(statusCode, parsedResponse);
+        }
+        catch(e){
+          callback(statusCode, false);
+        }
+      }
+    }
+  };
+
+  // Send the payload as JSON
+  let payloadString = JSON.stringify(payload);
+  xhr.send(payloadString);
 
 };
